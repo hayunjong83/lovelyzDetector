94c53d48fd7477d958d81535f51742de066c8c99
hayunjong83 <hayunjong83@gmail.com>
2021-10-26 19:37:01 +0900

    Crawling dataset using chromedrive

--- crawl/crawling.py
+++ crawl/crawling.py
@@ -60,7 +60,7 @@ def crawl(search_term, name):
             time.sleep(SCROLL_PAUSE_TIME)
             
             imgUrl = driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img').get_attribute("src")
-            request.urlretrieve(imgUrl, os.path.join(name, str(count)+".jpg"))
+            request.urlretrieve(imgUrl, os.path.join('lovelyz', name, str(count)+".jpg"))
             count = count + 1
         except :
             pass
